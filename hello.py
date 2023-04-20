ba0f4970f199d0953318a6b23877a0cc24cf2b8c
Casu3475 <106447335+Casu3475@users.noreply.github.com>
2023-04-20 20:49:28 +0200

    ok

--- hello.py
+++ hello.py
@@ -17,6 +17,38 @@
 # total_tax = bill * tax_rate / 100
 # print('Total tax', total_tax)
 
-def calculate_tax(bill, tax_rate):
-    return bill * tax_rate / 100
+# def calculate_tax(bill, tax_rate):
+#     return bill * tax_rate / 100
     
+# print('Total tax', calculate_tax(1000, 8))
+
+# def sum_of(*args):
+#     sum = 0
+#     for x in args:
+#         sum += x
+#     return sum
+
+# print (sum_of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+
+# nums = 34
+# for i in nums:
+#     print(i)
+
+# list_items = [10, 22, 45, 67, 90]
+# print(list_items[2])
+
+# new_list = [1,2,3,4]
+# new_list.insert(0,0)
+
+# print(new_list)
+
+def isPalindrome(str):
+    startIndex = 0
+    endIndex = len(str) - 1
+
+    for x in str:
+        if str[startIndex] != str[endIndex]:
+            return False
+    return True
+
+print(isPalindrome('racecar'))
